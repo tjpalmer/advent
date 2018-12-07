5db07dfb6fba84b4f5cba3a2e0bb221fbde29d2b
Tom <tjpalmer@tjpalmer.com>
2018-12-07 05:39:06 -0700

    Convert errors

--- 2018/01/rust/repeat/src/main.rs
+++ 2018/01/rust/repeat/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 
-fn main() -> io::Result<()> {
+fn main() -> Try<()> {
     let values = read_values()?;
     let mut prevs = HashSet::new();
     let mut total = 0;
@@ -19,11 +19,30 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn read_values() -> io::Result<Vec<i32>> {
+fn read_values() -> Try<Vec<i32>> {
     let file = File::open("../../input/input.txt")?;
     let mut values = Vec::new();
     for line in BufReader::new(file).lines() {
-        values.push(line.unwrap().parse::<i32>().unwrap());
+        values.push(line?.parse::<i32>()?);
     }
     Ok(values)
+}
+
+#[derive(Debug)]
+pub struct Err {
+    pub message: String,
+}
+
+type Try<Value> = Result<Value, Err>;
+
+impl From<io::Error> for Err {
+    fn from(_: io::Error) -> Err {
+        Err{message: "io error".to_string()}
+    }
+}
+
+impl From<std::num::ParseIntError> for Err {
+    fn from(_: std::num::ParseIntError) -> Err {
+        Err{message: "parse int error".to_string()}
+    }
 }
