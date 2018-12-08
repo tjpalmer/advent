f53111d0dac9e2c7a7311d2c22a546cbea167a7d
Tom <tjpalmer@tjpalmer.com>
2018-12-08 14:19:58 -0700

    Clean up rust checksum

--- 2018/02/rust/checksum/src/main.rs
+++ 2018/02/rust/checksum/src/main.rs
@@ -2,13 +2,13 @@ use std::io::{self, BufRead, BufReader};
 use std::fs::File;
 
 fn main() -> Try<()> {
-    let file = File::open("../../input/input.txt")?;
     let mut count2 = 0;
     let mut count3 = 0;
+    let file = File::open("../../input/input.txt")?;
     for line in BufReader::new(file).lines() {
         // Count chars.
         let mut counts = [0; 256];
-        for c in line?.bytes() {
+        for c in line?.chars() {
             counts[c as usize] += 1;
         }
         // Find 2s and 3s.
@@ -29,11 +29,5 @@ type Try<Value> = Result<Value, Err>;
 impl From<io::Error> for Err {
     fn from(_: io::Error) -> Err {
         Err{message: "io error".to_string()}
-    }
-}
-
-impl From<std::num::ParseIntError> for Err {
-    fn from(_: std::num::ParseIntError) -> Err {
-        Err{message: "parse int error".to_string()}
     }
 }
