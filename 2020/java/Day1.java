18e4dc4a3fa998abd09c5baae6a700d1524c1e3f
Tom <tom@contextfree.info>
2021-04-09 23:17:48 -0700

    Start 2020 java

---
+++ 2020/java/Day1.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Day1 {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("Hi %s\n", args[0]);
+        int[] numbers;
+        try (var lineStream = Files.lines(new File(args[0]).toPath())) {
+            numbers = lineStream.mapToInt(Integer::parseInt).toArray();
+        }
+        IntStream
+            .range(0, numbers.length)
+            .filter(i -> {
+                var a = numbers[i];
+                // Arrays.stream(numbers).skip(i + 1);
+                var optionalB = IntStream
+                    .range(i + 1, numbers.length)
+                    .filter(j -> a + numbers[j] == 2020)
+                    .findFirst();
+                optionalB.ifPresent(j -> System.out.println(a * numbers[j]));
+                return optionalB.isPresent();
+            })
+            .findFirst();
+    }
+}
