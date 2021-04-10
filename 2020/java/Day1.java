f96fe0954fe8a41055bc1338f88e166194638b21
Tom <tom@contextfree.info>
2021-04-10 06:36:32 -0700

    Make more functional

--- 2020/java/Day1.java
+++ 2020/java/Day1.java
@@ -1,26 +1,28 @@
 import java.io.File;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 class Day1 {
     public static void main(String[] args) throws Exception {
-        System.out.printf("Hi %s\n", args[0]);
         int[] numbers;
         try (var lineStream = Files.lines(new File(args[0]).toPath())) {
             numbers = lineStream.mapToInt(Integer::parseInt).toArray();
         }
-        IntStream
+        var ab = IntStream
             .range(0, numbers.length)
-            .filter(i -> {
+            .mapToObj(i -> {
                 var a = numbers[i];
-                var optionalB = IntStream
+                return IntStream
                     .range(i + 1, numbers.length)
-                    .filter(j -> a + numbers[j] == 2020)
-                    .findFirst();
-                optionalB.ifPresent(j -> System.out.println(a * numbers[j]));
-                return optionalB.isPresent();
+                    .map(j -> numbers[j])
+                    .filter(b -> a + b == 2020)
+                    .mapToObj(b -> new int[]{a, b})
+                    .findAny()
+                    .orElse(null);
             })
-            .findFirst();
+            .filter(ab2 -> ab2 != null)
+            .findAny()
+            .orElseThrow();
+        System.out.printf("%s * %s == %s\n", ab[0], ab[1], ab[0] * ab[1]);
     }
 }
